750338c4b4bf1c49ec659c8baa8857a85b3711f2
Solarflame5 <47642904+Solarflame5@users.noreply.github.com>
2022-05-31 19:13:37 +0300

    Updated main.py and README.md
    - fixed !xkcd [NaN] crashing the bot
    - added timestamp and author to the embed
    - made the embed look nicer
    - seperated `scrapeXKCD()` into two functions to make adding title search easier in the future
    - tidied up the code

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-# NOTE: THIS CODE IS WORK IN PROGRESS, DO NOT USE THIS CODE, IT'S TERRIBLE
-
 import discord
 from discord.ext import commands
 import requests
-import json
+import json # Used to turn xkcd's comic .json into a python dictionary
+import datetime # Used to format comic date for the embed timestamp
 
-def scrapeXKCD(xkcd_num = None):
-    xkcd_url = None
-    xkcd_json = None
-    xkcd_raw = None
-    xkcd = None
-    if xkcd_num == None: # if no number has been given, scrape latest comic.
+def findXkcdUrl(command_input): # Generate the URL to scrape from the command input
+    if command_input == None: # If no input has been given, scrape latest comic.
         xkcd_url = "https://www.xkcd.com/info.0.json"
     else:
+        xkcd_num = int(command_input)
         xkcd_url = "https://www.xkcd.com/" + str(xkcd_num) + "/info.0.json"
+    return xkcd_url
+
+def scrapeXKCD(xkcd_url):
     xkcd_json = requests.get(xkcd_url) # TEMPORARY! will switch to aiohttp
     xkcd_raw = json.loads(xkcd_json.content) # parse json into dictionary
+    date = datetime.datetime(int(xkcd_raw["year"]), int(xkcd_raw["month"]), int(xkcd_raw["day"])) # generate date
     xkcd_content = {
         "title": xkcd_raw["title"],
         "img": xkcd_raw["img"],
-        "alt": xkcd_raw["alt"]
+        "alt": xkcd_raw["alt"],
+        "date": date,
+        "url": "https://www.xkcd.com/" + str(xkcd_raw["num"])
     } # only return the needed content
     return xkcd_content
 
-# read bot token from "token.txt" in the same folder as "main.py"
+# Read bot token from "token.txt" in the same folder as "main.py"
 token_file = open("token.txt")
 bot_token = token_file.read()
 token_file.close()
@@ -37,7 +39,21 @@ async def on_ready():
 
 @client.command()
 async def xkcd(comic, number=None):
-    requested_comic = scrapeXKCD(number)
-    await comic.send(embed=discord.Embed(title=requested_comic["title"], description=requested_comic["alt"]).set_image(url=requested_comic["img"]))
+    try: # Check if input is valid
+        xkcd_url = findXkcdUrl(number)
+    except ValueError:
+        await comic.send("Invalid input, please only use\n```\n!xkcd\n!xkcd [number]\n```")
+        return # Abort command if input is invalid
+    requested_comic = scrapeXKCD(xkcd_url)
+    comic_embed = discord.Embed(
+        title=requested_comic["title"], # Comic title
+        description=requested_comic["alt"], # Comic hover text
+        url=requested_comic["url"], # Comic URL
+        timestamp=requested_comic["date"], # Comic date
+        colour=discord.Colour.from_rgb(150, 168, 200) # xkcd.com's background color
+    )
+    comic_embed.set_image(url=requested_comic["img"])
+    comic_embed.set_footer(text="Randall Munroe")
+    await comic.send(embed=comic_embed)
 
 client.run(bot_token)
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
-# discord XKCD bot
+# Discord XKCD bot
 this bot scrapes an xkcd comic with the given number and sends it in chat
-
-THIS PROJECT IS WORK IN PROGRESS AND THE CODE IS VERY UGLY, PLEASE DO NOT LOOK AT IT
-this is my first time making a discord bot and i know absolutely nothing about discord.py
+## Usage
+type `!xkcd` or `!xkcd [number]` in chat.
+## Disclaimer
+this is my first time making a discord bot and i know absolutely nothing about discord.py, feel free to criticise my code
