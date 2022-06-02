ef511f1d4e7d20a078925b1f0b7bc840068528b3
Solarflame5 <47642904+Solarflame5@users.noreply.github.com>
2022-06-02 19:34:12 +0300

    Implement slash commands and more
    - Switched to slash commands
    - Fixed sending a comic that doesn't exist causing issues
    - Added a screenshot to readme.md and polished it more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord import app_commands
 import requests
 import json # Used to turn xkcd's comic .json into a python dictionary
 import datetime # Used to format comic date for the embed timestamp
@@ -14,6 +14,8 @@ def findXkcdUrl(command_input): # Generate the URL to scrape from the command in
 
 def scrapeXKCD(xkcd_url):
     xkcd_json = requests.get(xkcd_url) # TEMPORARY! will switch to aiohttp
+    if xkcd_json.status_code != 200:
+        raise Exception("ComicNotFound")
     xkcd_raw = json.loads(xkcd_json.content) # parse json into dictionary
     date = datetime.datetime(int(xkcd_raw["year"]), int(xkcd_raw["month"]), int(xkcd_raw["day"])) # generate date
     xkcd_content = {
@@ -30,21 +32,30 @@ token_file = open("token.txt")
 bot_token = token_file.read()
 token_file.close()
 
-client = discord.Client()
-client = commands.Bot(command_prefix="!")
+intents = discord.Intents.default()
+intents.message_content = False
+
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
     print('logged in as {0.user}'.format(client))
 
-@client.command()
-async def xkcd(comic, number=None):
-    try: # Check if input is valid
-        xkcd_url = findXkcdUrl(number)
-    except ValueError:
-        await comic.send("Invalid input, please only use\n```\n!xkcd\n!xkcd [number]\n```")
-        return # Abort command if input is invalid
-    requested_comic = scrapeXKCD(xkcd_url)
+@tree.command(name="xkcd", description="Send an xkcd comic in chat.")
+@app_commands.describe(number="The comic's number")
+async def sendComic(interaction: discord.Interaction, number: int=None):
+    xkcd_url = findXkcdUrl(number)
+    try:
+        requested_comic = scrapeXKCD(xkcd_url)
+    except:
+        error_embed = discord.Embed(
+            title="Invalid input", 
+            description="The comic you tried to send does not exist.", 
+            colour=discord.Colour.from_rgb(200, 150, 150)
+        )
+        await interaction.response.send_message(embed=error_embed, ephemeral=True)
+        return
     comic_embed = discord.Embed(
         title=requested_comic["title"], # Comic title
         description=requested_comic["alt"], # Comic hover text
@@ -54,6 +65,6 @@ async def xkcd(comic, number=None):
     )
     comic_embed.set_image(url=requested_comic["img"])
     comic_embed.set_footer(text="Randall Munroe")
-    await comic.send(embed=comic_embed)
+    await interaction.response.send_message(embed=comic_embed, ephemeral=False)
 
 client.run(bot_token)
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 # Discord XKCD bot
-this bot scrapes an xkcd comic with the given number and sends it in chat
+This bot scrapes an [xkcd](https://xkcd.com/) comic with the given number and sends it in chat
+![Example screenshot](screenshots/example.png)
+## Requirements
+- [discord.py](https://github.com/Rapptz/discord.py)'s development version.
+- [Requests](https://github.com/psf/requests)
 ## Usage
-type `!xkcd` or `!xkcd [number]` in chat.
+type `/xkcd` or `/xkcd [number]` in chat.
 ## Disclaimer
-this is my first time making a discord bot and i know absolutely nothing about discord.py, feel free to criticise my code
+this is my first time making a discord bot and i know absolutely nothing about [discord.py](https://github.com/Rapptz/discord.py), feel free to criticise my code
